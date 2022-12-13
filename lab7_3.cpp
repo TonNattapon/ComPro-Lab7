3d808dc19ee152a9c97f84fb8e540596caada894
= <=>
2022-12-13 16:56:10 +0700

    Complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,36 @@
 #include<iostream>
-
 using namespace std;
+
+double adiff(int a,int b){
+  int c;
+  a = a % 360, b = b % 360;
+  c = a - b;
+    if (c < 0){
+     c = c * -1;
+    }
+    if (c > 180)
+    {
+      c = 360 - c;      
+    }
+    return c;
+}
 
 
 int main(){
   cout << adiff(180,270);
+  cout << endl;
   cout << adiff(210,45);
+  cout << endl;
   cout << adiff(0,360);
+  cout << endl;
   cout << adiff(10,350);
+  cout << endl;
   cout << adiff(95,260);
+  cout << endl;
   cout << adiff(90,-90);
+  cout << endl;
   cout << adiff(1000,280);
+  cout << endl;
   cout << adiff(60,244);
+  cout << endl;
 }
